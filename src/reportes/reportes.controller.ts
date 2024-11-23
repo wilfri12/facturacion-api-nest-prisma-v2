aeaba09525e8daa1bad929aa350df4b7498ee3c6
wilfri <wilfrip15@gmail.com>
2024-11-22 20:38:44 -0400

    Se procedio a comentar codigo que no se usa

--- src/reportes/reportes.controller.ts
+++ src/reportes/reportes.controller.ts
@@ -24,64 +24,64 @@ export class ReportesController {
     }
   }
 
-  // Endpoint para obtener productos sin stock
-  @UseGuards(AuthGuard)
-  @Get('productos-sin-stock')
-  async obtenerProductosSinStock(
-    @Query('pagina') pagina: number = 1,
-    @Query('limite') limite: number = 10
-  ) {
-    try {
-      const result = await this.reportesService.obtenerProductosSinStock(pagina, limite);
-      return result;
-    } catch (error) {
-      console.error('Error al obtener productos sin stock:', error);
-      return {
-        success: false,
-        message: 'Error al obtener productos sin stock.',
-        error: error.message,
-      };
-    }
-  }
+  // // Endpoint para obtener productos sin stock
+  // @UseGuards(AuthGuard)
+  // @Get('productos-sin-stock')
+  // async obtenerProductosSinStock(
+  //   @Query('pagina') pagina: number = 1,
+  //   @Query('limite') limite: number = 10
+  // ) {
+  //   try {
+  //     const result = await this.reportesService.obtenerProductosSinStock(pagina, limite);
+  //     return result;
+  //   } catch (error) {
+  //     console.error('Error al obtener productos sin stock:', error);
+  //     return {
+  //       success: false,
+  //       message: 'Error al obtener productos sin stock.',
+  //       error: error.message,
+  //     };
+  //   }
+  // }
 
-  // Endpoint para obtener productos con bajo stock
-  @UseGuards(AuthGuard)
-  @Get('productos-bajo-stock')
-  async obtenerProductosBajoStock(
-    @Query('umbral') umbral: number = 10,
-    @Query('page') page: number = 1,
-    @Query('limit') limit: number = 10
-  ) {
-    try {
-      const result = await this.reportesService.obtenerProductosBajoStock({ umbral, page, limit,  });
-      return result;
-    } catch (error) {
-      console.error('Error al obtener productos con bajo stock:', error);
-      return {
-        success: false,
-        message: 'Error al obtener productos con bajo stock.',
-        error: error.message,
-      };
-    }
-  }
+  // // Endpoint para obtener productos con bajo stock
+  // @UseGuards(AuthGuard)
+  // @Get('productos-bajo-stock')
+  // async obtenerProductosBajoStock(
+  //   @Query('umbral') umbral: number = 10,
+  //   @Query('page') page: number = 1,
+  //   @Query('limit') limit: number = 10
+  // ) {
+  //   try {
+  //     const result = await this.reportesService.obtenerProductosBajoStock({ umbral, page, limit,  });
+  //     return result;
+  //   } catch (error) {
+  //     console.error('Error al obtener productos con bajo stock:', error);
+  //     return {
+  //       success: false,
+  //       message: 'Error al obtener productos con bajo stock.',
+  //       error: error.message,
+  //     };
+  //   }
+  // }
 
 
-  // Endpoint para calcular el valor total del inventario
-  @UseGuards(AuthGuard)
-  @Get('valor-inventario-total')
-  async calcularValorInventarioTotal() {
-    try {
-      const result = await this.reportesService.calcularValorInventarioTotal();
-      return result;
-    } catch (error) {
-      console.error('Error al calcular el valor total del inventario:', error);
-      return {
-        success: false,
-        message: 'Error al calcular el valor total del inventario.',
-        error: error.message,
-      };
-    }
-  }
+  // // Endpoint para calcular el valor total del inventario
+  // @UseGuards(AuthGuard)
+  // @Get('valor-inventario-total')
+  // async calcularValorInventarioTotal() {
+  //   try {
+  //     const result = await this.reportesService.calcularValorInventarioTotal();
+  //     return result;
+  //   } catch (error) {
+  //     console.error('Error al calcular el valor total del inventario:', error);
+  //     return {
+  //       success: false,
+  //       message: 'Error al calcular el valor total del inventario.',
+  //       error: error.message,
+  //     };
+  //   }
+  // }
 
   // Endpoint para calcular la utilidad bruta en un periodo
   @UseGuards(AuthGuard)
@@ -137,61 +137,61 @@ async calcularDatosDashboard(
 
   
 
-  // Endpoint para obtener productos más vendidos
-  @UseGuards(AuthGuard)
-  @Get('productos-mas-vendidos')
-  async obtenerProductosMasVendidos(
-    @Query('periodo') periodo: 'semana' | 'mes',
-    @Query('limite') limite: number = 10
-  ) {
-    try {
-      const result = await this.reportesService.obtenerProductosMasVendidos(periodo, limite);
-      return result;
-    } catch (error) {
-      console.error('Error al obtener productos más vendidos:', error);
-      return {
-        success: false,
-        message: 'Error al obtener productos más vendidos.',
-        error: error.message,
-      };
-    }
-  }
+  // // Endpoint para obtener productos más vendidos
+  // @UseGuards(AuthGuard)
+  // @Get('productos-mas-vendidos')
+  // async obtenerProductosMasVendidos(
+  //   @Query('periodo') periodo: 'semana' | 'mes',
+  //   @Query('limite') limite: number = 10
+  // ) {
+  //   try {
+  //     const result = await this.reportesService.obtenerProductosMasVendidos(periodo, limite);
+  //     return result;
+  //   } catch (error) {
+  //     console.error('Error al obtener productos más vendidos:', error);
+  //     return {
+  //       success: false,
+  //       message: 'Error al obtener productos más vendidos.',
+  //       error: error.message,
+  //     };
+  //   }
+  // }
 
-  // Endpoint para obtener resumen de facturas por estado
-  @UseGuards(AuthGuard)
-  @Get('resumen-facturas-estado')
-  async resumenFacturasPorEstado(
-    @Query('year') year: number
-  ) {
-    try {
-      const result = await this.reportesService.resumenFacturasPorEstado(year);
-      return result;
-    } catch (error) {
-      console.error('Error al obtener resumen de facturas por estado:', error);
-      return {
-        success: false,
-        message: 'Error al obtener resumen de facturas por estado.',
-        error: error.message,
-      };
-    }
-  }
+  // // Endpoint para obtener resumen de facturas por estado
+  // @UseGuards(AuthGuard)
+  // @Get('resumen-facturas-estado')
+  // async resumenFacturasPorEstado(
+  //   @Query('year') year: number
+  // ) {
+  //   try {
+  //     const result = await this.reportesService.resumenFacturasPorEstado(year);
+  //     return result;
+  //   } catch (error) {
+  //     console.error('Error al obtener resumen de facturas por estado:', error);
+  //     return {
+  //       success: false,
+  //       message: 'Error al obtener resumen de facturas por estado.',
+  //       error: error.message,
+  //     };
+  //   }
+  // }
 
-  // Endpoint para obtener datos de ventas y compras mensuales para el año especificado
-  @UseGuards(AuthGuard)
-  @Get('ventas-compras-mensuales')
-  async obtenerDatosVentasComprasMensuales(
-    @Query('year') year: number
-  ) {
-    try {
-      const result = await this.reportesService.obtenerDatosVentasComprasMensuales(year);
-      return result;
-    } catch (error) {
-      console.error('Error al obtener datos de ventas y compras mensuales:', error);
-      return {
-        success: false,
-        message: 'Error al obtener datos de ventas y compras mensuales.',
-        error: error.message,
-      };
-    }
-  }
+  // // Endpoint para obtener datos de ventas y compras mensuales para el año especificado
+  // @UseGuards(AuthGuard)
+  // @Get('ventas-compras-mensuales')
+  // async obtenerDatosVentasComprasMensuales(
+  //   @Query('year') year: number
+  // ) {
+  //   try {
+  //     const result = await this.reportesService.obtenerDatosVentasComprasMensuales(year);
+  //     return result;
+  //   } catch (error) {
+  //     console.error('Error al obtener datos de ventas y compras mensuales:', error);
+  //     return {
+  //       success: false,
+  //       message: 'Error al obtener datos de ventas y compras mensuales.',
+  //       error: error.message,
+  //     };
+  //   }
+  // }
 }
--- src/reportes/reportes.service.ts
+++ src/reportes/reportes.service.ts
@@ -86,138 +86,138 @@ export class ReportesService {
     };
   }
 
-  // Servicio que obtiene productos sin stock obtenerProductosSinStock
-  async obtenerProductosSinStock(page: number = 1, limit: number = 10) {
-    const validatedPage = Math.max(1, page);
-    const validatedLimit = Math.max(1, limit);
+  // // Servicio que obtiene productos sin stock obtenerProductosSinStock
+  // async obtenerProductosSinStock(page: number = 1, limit: number = 10) {
+  //   const validatedPage = Math.max(1, page);
+  //   const validatedLimit = Math.max(1, limit);
 
-    try {
-      const [productos, totalRecords] = await Promise.all([
-        this.prisma.producto.findMany({
-          where: { stock: 0 },
-          select: {
-            codigo: true,
-            nombre: true,
-            stock: true,
-            categoria: { select: { nombre: true } },
-            subCategoria: { select: { nombre: true } },
-          },
-          skip: (validatedPage - 1) * validatedLimit,
-          take: validatedLimit,
-        }),
-        this.prisma.producto.count({ where: { stock: 0 } })
-      ]);
+  //   try {
+  //     const [productos, totalRecords] = await Promise.all([
+  //       this.prisma.producto.findMany({
+  //         where: { stock: 0 },
+  //         select: {
+  //           codigo: true,
+  //           nombre: true,
+  //           stock: true,
+  //           categoria: { select: { nombre: true } },
+  //           subCategoria: { select: { nombre: true } },
+  //         },
+  //         skip: (validatedPage - 1) * validatedLimit,
+  //         take: validatedLimit,
+  //       }),
+  //       this.prisma.producto.count({ where: { stock: 0 } })
+  //     ]);
 
-      const totalPages = Math.max(1, Math.ceil(totalRecords / validatedLimit));
-      const hasNext = validatedPage < totalPages;
+  //     const totalPages = Math.max(1, Math.ceil(totalRecords / validatedLimit));
+  //     const hasNext = validatedPage < totalPages;
 
-      return {
-        success: true,
-        data: {
-          productos,
-          totalRecords,
-          limit: validatedLimit,
-          currentPage: validatedPage,
-          totalPages,
-          hasNext,
-        },
-        message: 'Consulta satisfactoria',
-      };
-    } catch (error) {
-      throw new Error(`Error al obtener productos sin stock: ${error.message}`);
-    }
-  }
+  //     return {
+  //       success: true,
+  //       data: {
+  //         productos,
+  //         totalRecords,
+  //         limit: validatedLimit,
+  //         currentPage: validatedPage,
+  //         totalPages,
+  //         hasNext,
+  //       },
+  //       message: 'Consulta satisfactoria',
+  //     };
+  //   } catch (error) {
+  //     throw new Error(`Error al obtener productos sin stock: ${error.message}`);
+  //   }
+  // }
 
-  // Servicio que obtiene productos con bajo stock 
-  async obtenerProductosBajoStock(params: { umbral: number, page: number, limit: number }) {
-    const { umbral = 10, page = 1, limit = 10 } = params;
-    const validatedPage = Math.max(1, page);
-    const validatedLimit = Math.max(1, limit);
-    const validatedUmbral = Math.max(1, umbral);
+  // // Servicio que obtiene productos con bajo stock 
+  // async obtenerProductosBajoStock(params: { umbral: number, page: number, limit: number }) {
+  //   const { umbral = 10, page = 1, limit = 10 } = params;
+  //   const validatedPage = Math.max(1, page);
+  //   const validatedLimit = Math.max(1, limit);
+  //   const validatedUmbral = Math.max(1, umbral);
 
-    console.log('Bajo stock', params);
-
-
-    try {
-      const [productos, totalRecords] = await Promise.all([
-        this.prisma.producto.findMany({
-          where: { stock: { lt: validatedUmbral, gt: 0 } },
-          select: {
-            codigo: true,
-            nombre: true,
-            stock: true,
-            categoria: { select: { nombre: true } },
-            subCategoria: { select: { nombre: true } },
-          },
-          skip: (validatedPage - 1) * validatedLimit,
-          take: validatedLimit,
-        }),
-        this.prisma.producto.count({ where: { stock: { lt: validatedUmbral, gt: 0 } } }),
-      ]);
-
-      const totalPages = Math.max(1, Math.ceil(totalRecords / validatedLimit));
-      const hasNext = validatedPage < totalPages;
-
-      return {
-        success: true,
-        data: {
-          productos,
-          totalRecords,
-          limit: validatedLimit,
-          currentPage: validatedPage,
-          totalPages,
-          hasNext,
-        },
-        message: 'Consulta satisfactoria',
-      };
-    } catch (error) {
-      throw new Error(`Error al obtener productos con bajo stock: ${error.message}`);
-    }
-  }
+  //   console.log('Bajo stock', params);
 
 
-  // Servicio que calcula el valor total del inventario
-  async calcularValorInventarioTotal() {
-    try {
+  //   try {
+  //     const [productos, totalRecords] = await Promise.all([
+  //       this.prisma.producto.findMany({
+  //         where: { stock: { lt: validatedUmbral, gt: 0 } },
+  //         select: {
+  //           codigo: true,
+  //           nombre: true,
+  //           stock: true,
+  //           categoria: { select: { nombre: true } },
+  //           subCategoria: { select: { nombre: true } },
+  //         },
+  //         skip: (validatedPage - 1) * validatedLimit,
+  //         take: validatedLimit,
+  //       }),
+  //       this.prisma.producto.count({ where: { stock: { lt: validatedUmbral, gt: 0 } } }),
+  //     ]);
 
-      const productosInventory = await this.prisma.producto.findMany({
-        where: { stock: { gt: 0 } },
-        select: {
-          precio: true,
-          stock: true,
-        },
-      });
+  //     const totalPages = Math.max(1, Math.ceil(totalRecords / validatedLimit));
+  //     const hasNext = validatedPage < totalPages;
 
-      const valorTotal = productosInventory.reduce((acc, producto) => acc + Number(producto.precio) * producto.stock, 0);
+  //     return {
+  //       success: true,
+  //       data: {
+  //         productos,
+  //         totalRecords,
+  //         limit: validatedLimit,
+  //         currentPage: validatedPage,
+  //         totalPages,
+  //         hasNext,
+  //       },
+  //       message: 'Consulta satisfactoria',
+  //     };
+  //   } catch (error) {
+  //     throw new Error(`Error al obtener productos con bajo stock: ${error.message}`);
+  //   }
+  // }
 
-      const productos = await this.prisma.producto.findMany({
-        where: { stock: { gt: 0 } },
-        select: {
-          precio: true,
-          stock: true,
-          categoria: { select: { nombre: true } },
-        },
-      });
 
-      const valorPorCategoria = productos.reduce((acc, producto) => {
-        const categoria = producto.categoria.nombre;
-        const valor = Number(producto.precio) * producto.stock;
-        acc[categoria] = (acc[categoria] || 0) + valor;
-        return acc;
-      }, {} as Record<string, number>);
+  // // Servicio que calcula el valor total del inventario
+  // async calcularValorInventarioTotal() {
+  //   try {
 
-      return {
-        success: true,
-        data: {
-          valorTotal,
-          valorPorCategoria,
-        },
-        message: 'Consulta satisfactoria',
-      };
-    } catch (error) {
-      throw new Error(`Error al obtener el valor de inventario por categoría: ${error.message}`);
-    }
-  }
+  //     const productosInventory = await this.prisma.producto.findMany({
+  //       where: { stock: { gt: 0 } },
+  //       select: {
+  //         precio: true,
+  //         stock: true,
+  //       },
+  //     });
+
+  //     const valorTotal = productosInventory.reduce((acc, producto) => acc + Number(producto.precio) * producto.stock, 0);
+
+  //     const productos = await this.prisma.producto.findMany({
+  //       where: { stock: { gt: 0 } },
+  //       select: {
+  //         precio: true,
+  //         stock: true,
+  //         categoria: { select: { nombre: true } },
+  //       },
+  //     });
+
+  //     const valorPorCategoria = productos.reduce((acc, producto) => {
+  //       const categoria = producto.categoria.nombre;
+  //       const valor = Number(producto.precio) * producto.stock;
+  //       acc[categoria] = (acc[categoria] || 0) + valor;
+  //       return acc;
+  //     }, {} as Record<string, number>);
+
+  //     return {
+  //       success: true,
+  //       data: {
+  //         valorTotal,
+  //         valorPorCategoria,
+  //       },
+  //       message: 'Consulta satisfactoria',
+  //     };
+  //   } catch (error) {
+  //     throw new Error(`Error al obtener el valor de inventario por categoría: ${error.message}`);
+  //   }
+  // }
 
   // Servicio que calcula utilidad bruta en un periodo
   async calcularDatosDashboard(periodoInicio: Date, periodoFin: Date): Promise<{ success: boolean; data: DashboardData }> {
@@ -388,144 +388,6 @@ export class ReportesService {
       },
     };
   }
-  
-  
-  
-
-
-  
-
-
-  // Servicio que obtiene productos más vendidos
-  async obtenerProductosMasVendidos(periodo: 'semana' | 'mes', limite: number = 10): Promise<ApiResponse<{ producto: string; categoria: string; totalVentas: number }[]>> {
-    const fechaInicio = this.calcularFechaInicio(periodo);
-
-    console.log('obtenerProductosMasVendidos', periodo);
-    console.log('obtenerProductosMasVendidos', fechaInicio);
-
-    // Agrupamos los detalles de factura por producto y sumamos las cantidades
-    const productosVendidos = await this.prisma.detalleFactura.groupBy({
-      by: ['productoId'],
-      _sum: { cantidad: true },
-      where: {
-        factura: { createdAt: { gte: fechaInicio } }
-      },
-      orderBy: {
-        _sum: { cantidad: 'desc' }
-      },
-      take: Number(limite)
-    });
-
-    // Mapeamos los resultados para obtener los nombres de productos y categorías
-    const productosConDetalles = await Promise.all(
-      productosVendidos.map(async (producto) => {
-        const detallesProducto = await this.prisma.producto.findUnique({
-          where: { id: producto.productoId },
-          select: {
-            nombre: true,
-            categoria: {
-              select: { nombre: true }
-            }
-          }
-        });
-        return {
-          producto: detallesProducto?.nombre || 'Producto desconocido',
-          categoria: detallesProducto?.categoria.nombre || 'Sin categoría',
-          totalVentas: producto._sum.cantidad || 0
-        };
-      })
-    );
-
-    return {
-      success: true,
-      data: productosConDetalles
-    };
-  }
-
-
-  // Servicio que obtiene resumen de facturas por estado
-  async resumenFacturasPorEstado(year: number): Promise<ApiResponse<{ facturasEmitidas: number; totalEmitido: number; facturasPagadas: number; totalPagado: number; facturasPendientes: number; totalPendiente: number }>> {
-    const inicioAño = new Date(`${Number(year)}-01-01`);
-    const finAño = new Date(`${Number(year) + 1}-01-01`);
-
-    console.log('resumenFacturasPorEstado', inicioAño);
-    console.log('resumenFacturasPorEstado', finAño);
-
-    const facturasEmitidas = await this.prisma.factura.aggregate({
-      _count: { id: true },
-      _sum: { total: true },
-      where: { createdAt: { gte: inicioAño, lt: finAño } }
-    });
-    const facturasPagadas = await this.prisma.factura.aggregate({
-      _count: { id: true },
-      _sum: { total: true },
-      where: { estado: 'PAGADA', createdAt: { gte: inicioAño, lt: finAño } }
-    });
-
-    const facturasPendientes = (facturasEmitidas._count.id || 0) - (facturasPagadas._count.id || 0);
-    const totalPendiente = (Number(facturasEmitidas._sum.total) || 0) - (Number(facturasPagadas._sum.total) || 0);
-
-    return {
-      success: true,
-      data: {
-        facturasEmitidas: facturasEmitidas._count.id || 0,
-        totalEmitido: Number(facturasEmitidas._sum.total) || 0,
-        facturasPagadas: facturasPagadas._count.id || 0,
-        totalPagado: Number(facturasPagadas._sum.total) || 0,
-        facturasPendientes,
-        totalPendiente
-      }
-    };
-  }
-
-  // Servicio que obtiene datos de ventas y compras mensuales para el year especificado
-  async obtenerDatosVentasComprasMensuales(year: number): Promise<ApiResponse<{ etiquetas: string[]; datosVentas: number[]; datosCompras: number[] }>> {
-    const etiquetas = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-    const datosVentas = Array(12).fill(0);
-    const datosCompras = Array(12).fill(0);
-
-    // Obtener ventas agrupadas por mes
-    const ventas = await this.prisma.factura.groupBy({
-      by: ['createdAt'],
-      _sum: { total: true },
-      where: {
-        createdAt: {
-          gte: new Date(`${Number(year)}-01-01`),
-          lt: new Date(`${Number(year) + 1}-01-01`)
-        }
-      }
-    });
-
-    // Distribuir ventas en el array de datosVentas basado en el mes
-    ventas.forEach(venta => {
-      const mes = new Date(venta.createdAt).getMonth();
-      datosVentas[mes] += Number(venta._sum.total) || 0;
-    });
-
-    // Obtener compras agrupadas por mes
-    const compras = await this.prisma.compra.groupBy({
-      by: ['createdAt'],
-      _sum: { total: true },
-      where: {
-        createdAt: {
-          gte: new Date(`${Number(year)}-01-01`),
-          lt: new Date(`${Number(year) + 1}-01-01`)
-        }, 
-        delete: false
-      }
-    });
-
-    // Distribuir compras en el array de datosCompras basado en el mes
-    compras.forEach(compra => {
-      const mes = new Date(compra.createdAt).getMonth();
-      datosCompras[mes] += Number(compra._sum.total) || 0;
-    });
-
-    return {
-      success: true,
-      data: { etiquetas, datosVentas, datosCompras }
-    };
-  }
 
   // Función auxiliar para calcular variación porcentual
   private calcularVariacion(totalHoy: number, totalAyer: number): number {
@@ -533,12 +395,5 @@ export class ReportesService {
     return ((totalHoy - totalAyer) / totalAyer) * 100;
   }
 
-  // Función auxiliar para calcular la fecha de inicio en función del período
-  private calcularFechaInicio(periodo: 'dia' | 'semana' | 'mes'): Date {
-    const fechaActual = new Date();
-    if (periodo === 'dia') return new Date(fechaActual.setDate(fechaActual.getDate() - 1));
-    if (periodo === 'semana') return new Date(fechaActual.setDate(fechaActual.getDate() - 7));
-    if (periodo === 'mes') return new Date(fechaActual.setMonth(fechaActual.getMonth() - 1));
-    return fechaActual;
-  }
+ 
 }
